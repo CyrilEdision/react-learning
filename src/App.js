a369048941802968310bc9ab6e2c8cd9f785e72a
CyrilEdision <vcyriledison@gmail.com>
2021-04-19 16:39:35 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React from "react";
+import { Link } from "react-router";
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          id: 1,
+          name: "cyril",
+          age: 25
+        },
+        {
+          id: 2,
+          name: "Jose",
+          age: 26
+        },
+        {
+          id: 3,
+          name: "Nisha",
+          age: 24
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Link to="/">Map()</Link>
+        <table>
+          <tbody>
+            <tr>
+              <td>Id</td>
+              <td>Name</td>
+              <td>Age</td>
+            </tr>
+            {this.state.data.map((person, i) => (
+              <tr key={i}>
+                <td>{person.id}</td>
+                <td>{person.name}</td>
+                <td>{person.age}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
+
+export default App;
